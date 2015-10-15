63b93c877c71fc9ecedc22eb8e93674eed8ff02d
Jair Junior <j.junior89@hotmail.com>
2015-10-15 20:16:13 -0300

    Script Login.php construído para efetuar logoff e redirecionar para index.php

--- _script/Logout.php
+++ _script/Logout.php
@@ -10,7 +10,16 @@
  *
  *----------------------------------------------------------------------------*/
 
+/* Declara seção do navegador */
+session_start();
 
+/* Limpa todas variáveis de seção */
+session_unset();
 
+/* Destrói seção */
+session_destroy();
+
+/* Redireciona usuário para página inicial de login*/
+header('Location: ../index.php');
 
 ?>
