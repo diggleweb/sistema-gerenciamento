64535f36d47f49329fb6fcc31c123dada9d0a097
Jair Junior <j.junior89@hotmail.com>
2015-10-15 03:43:22 -0300

    Primeiro commit: estrutura de pastas criada + arquivo modelo.php

---
+++ modelo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lan="pt-br">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Sistema de Gerenciamento</title>
+</head>
+
+<body>
+<div id="interface">
+
+
+
+
+</div>
+</body>
+
+</html>
