99fd0cd436496ea9ad47c4d75739988b61f286c2
Jair Junior <j.junior89@hotmail.com>
2015-10-17 17:05:19 -0300

    Página criada: usuario_listagem.php onde lista todos usuários cadastrados e dá a opção de editar ou excluir

--- usuario_listagem.php
+++ usuario_listagem.php
@@ -1,6 +1,9 @@
 <?php
 session_start("login");
 
+/* Inclui classe UsuarioDAO */
+require_once("_script/UsuarioDAO.php");
+
 /* Testa se a variável de sessão está setada corretamente */
 /* Caso contrário, gera erro acusando que o login não foi feito */
 if( !isset($_SESSION["nome_usuario_logado"]) )
@@ -19,7 +22,45 @@ if( !isset($_SESSION["nome_usuario_logado"]) )
 <body>
 <div id="interface">
 
-<a href="console.php">Voltar</a>
+	<h1>Manutenção de Usuários</h1>
+
+
+	<!-- Tabela que lista usuários cadastrados no sistema -->
+	<table>
+		<tr>
+			<th>Nome</th>
+			<th>E-mail</th>
+			<th>Permissão</th>
+			<th>Ação</th>
+		</tr>
+		<!-- Busca todos usuários cadastrados no baco-->
+		<?php  
+			$usuarioDao = new UsuarioDAO();
+			$lista = $usuarioDao->listar();
+		?>
+		<!-- Imprime na tabela em HTML os usuários utilizando o PHP -->
+		<?php foreach ($lista as $indice => $usuario) { ?>
+			<tr>
+				<td><?php echo $usuario->nome; ?></td>
+				<td><?php echo $usuario->email; ?></td>
+				<td><?php
+						if($usuario->admin == 0)
+							echo "Normal";
+						elseif($usuario->admin == 1)
+							echo "Admin";
+				?></td>
+				<!-- Imprime links para editar ou excluir usuário -->
+				<td>
+					<a href="usuario_formulario.php?acao=editar&idusuario=<?php echo $usuario->idusuario; ?>">Editar</a>
+					&nbsp;&nbsp;
+					<a href="usuario_formulario.php?acao=excluir&idusuario=<?php echo $usuario->idusuario; ?>">Excluir</a>
+				</td>
+			</tr>
+		<?php } ?>
+	</table>
+
+
+	<br/><br/><a href="console.php">Voltar</a>
 
 </div>
 </body>
