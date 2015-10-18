cea05069fe52e184f52e1e468bf25f59507bdcdd
Jair Junior <j.junior89@hotmail.com>
2015-10-18 14:03:09 -0200

    Script criado: UsuarioProcessamento.php processa a ação de inserir/editar/excluir do usuario_formulario.php

---
+++ _script/UsuarioProcessamento.php
@@ -0,0 +1,40 @@
+<?php
+//ini_set('error_reporting', E_ALL|E_STRICT);
+//ini_set('display_errors', 1);
+
+require_once ('UsuarioDAO.php');
+
+
+$usuarioDao = new UsuarioDAO();
+
+$acao = $_POST["nAcao"];
+echo $acao;
+
+if( $acao == "editar" || $acao == "cadastrar" ) {
+	$usuario = new Usuario();
+	$usuario->idusuario = $_POST["nIdUsuario"];
+	$usuario->nome = $_POST["nNome"];
+	$usuario->email = $_POST["nEmail"];
+	$usuario->senha = $_POST["nSenha"];
+	
+	if( $_POST["nPermissao"] == "normal" )
+		$usuario->admin = "0";
+	elseif( $_POST["nPermissao"] == "admin" )
+		$usuario->admin = "1";
+
+	if($acao == "editar")
+		$usuarioDao->atualizar($usuario);
+	else
+		$usuarioDao->inserir($usuario);
+}
+
+elseif($acao == "excluir"){
+	$usuarioDao->excluir( $_POST["nIdUsuario"] );
+}
+
+
+
+header("Location: ../usuario_listagem.php")
+
+
+?>
