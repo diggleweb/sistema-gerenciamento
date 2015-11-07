6d19c0ffcf460fdc295c04e798690ce8688f0705
Jair Junior <j.junior89@hotmail.com>
2015-11-06 22:51:48 -0200

    Criada Página Inicial do sistema (dashboard)

---
+++ _paginas/dashboard.php
@@ -0,0 +1,6 @@
+
+    Olá, <?php echo $_SESSION["nome_usuario_logado"]; ?>. Seja bem-vindo!<br/><br/>
+
+ 
+    <a href="produto_listagem.php">Estoque</a><br/>
+    <a href="categoria_listagem.php">Categorias</a><br/>
