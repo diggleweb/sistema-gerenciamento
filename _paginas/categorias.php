df49b7b47990d18ca13b03a53df1ec405b00dd0e
Jair Junior <j.junior89@hotmail.com>
2015-11-07 18:40:22 -0200

    Adicionado metodo que imprime a lista de categorias na pagina.

--- _paginas/categorias.php
+++ _paginas/categorias.php
@@ -1,3 +1,17 @@
-<div class="page-header text-center">
+<div class="page-header">
 	<h1>Categorias</h1>
 </div>
+
+<div>
+
+
+	<?php
+
+		$categoriaDao = new CategoriaDAO;
+		$categoriaDao->listarCategorias();
+
+	?>
+
+
+
+
